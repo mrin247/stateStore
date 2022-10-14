406c9e23e412601694dfc323ead08af1d24fbb44
Mrin <mrinmoym247@gmail.com>
2022-10-14 13:16:48 +0530

    Deployment chnaged to vercel

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -1,2 +1,3 @@
 //export const api = 'http://localhost:5000/api';
-export const api = 'https://statefront-server.herokuapp.com/api';
+//export const api = 'https://statefront-server.herokuapp.com/api';
+export const api = 'https://state-server-1rsq50mrq-mrin247.vercel.app/api';
